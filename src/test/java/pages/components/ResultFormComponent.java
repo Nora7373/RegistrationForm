517e374e0fe3a6953f6b4722f49293f39f5a59c9
nora <nora7373@mail.ru>
2023-11-25 23:08:57 +0300

    commit page objects

---
+++ src/test/java/pages/components/ResultFormComponent.java
@@ -0,0 +1,17 @@
+package pages.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ResultFormComponent {
+    private final SelenideElement tableResponsiveInput = $(".table-responsive");
+
+    public void checkResult(String key, String value) {
+        tableResponsiveInput.should(appear).$(byText(key)).sibling(0).shouldHave(text(value));
+    }
+    }
+
